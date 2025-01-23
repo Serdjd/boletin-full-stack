176aa68a679e9c2001230f77a5b449c23a85bb8b
Serdjd <sergiodobladomunoz@gmail.com>
2025-01-23 23:34:03 +0100

    UPD: terminado

--- src/app/api/habitos/route.js
+++ src/app/api/habitos/route.js
@@ -19,5 +19,88 @@ export async function GET() {
 }
 
 export async function PUT(request) {
-    
+    const {searchParams} = new URL(request.url)
+    const id = +searchParams.get("id")
+    const body = await request.json()
+
+    if(id && body) {
+        const {data,error} = await supabase
+        .from("habito")
+        .update(body)
+        .eq("id",id)
+
+        if(error) {
+            return new Response(JSON.stringify(error),{
+                status: 500,
+                headers: {"Content-type": "application/json"}
+            })
+        }
+
+        return new Response(JSON.stringify({message: "Habito actualizado"}), {
+            status: 201,
+            headers: {"Content-type": "application/json"}
+        })
+    }
+    return new Response(JSON.stringify({message: "Error"}),{
+        status: 500,
+        headers: {"Content-type": "application/json"}
+    })
+}
+
+export async function POST(request) {
+    const body = await request.json()
+    let date = body.fecha.split('/')
+    const fecha = new Date(date[2], date[1] - 1, date[0])
+    if(fecha >= new Date() && body.nombre) {
+        body.fecha = fecha.toLocaleDateString('en-CA')
+        const {data,error} = await supabase
+        .from("habito")
+        .insert(body) 
+
+        if(error) {
+            return new Response(JSON.stringify(error),{
+                status: 500,
+                headers: {"Content-type": "application/json"}
+            })
+        }
+
+        return new Response(JSON.stringify({message: "Habito añadido"}),{
+            status: 201,
+            headers: {"Content-type": "application/json"}
+        })
+    }
+    return new Response(JSON.stringify({message: "Faltan datos o no son correctos"}),{
+        status: 500,
+        headers: {"Content-type": "application/json"}
+    })
+}
+
+export async function DELETE(request) {
+    const {searchParams} = new URL(request.url)
+    const id = +searchParams.get("id")
+
+    if(id) {
+        const {data,error,count} = await supabase
+        .from("habito")
+        .delete()
+        .eq("id",id)
+        .eq("completado",true)
+
+        if(error) {
+            return new Response(JSON.stringify(error),{
+                status: 500,
+                headers: {"Content-type":"application/json"}
+            })
+        }
+        if(count > 0) {
+            return new Response(JSON.stringify({message: "Habito eliminado"}),{
+                status: 201,
+                headers: {"Content-type": "application/json"}
+            })
+        }
+    }
+    return new Response(JSON.stringify({message: "Error"}),{
+        status: 500,
+        headers: {"Content-type": "application/json"}
+    })
 }
--- src/app/productos/add/page.js
+++ src/app/productos/add/page.js
@@ -1,8 +1,20 @@
+'use client'
+
+import { useRef } from "react"
+
 export default function ProductForm() {
-    
+    const formRef = useRef()
+    async function addProduct() {
+        let message = ""
+        
+    }
     return(
-        <form ref={} action={}>
-            
+        <form ref={formRef} action={addProduct}>
+            <label>Nombre: <input name="nombre" type="text" required></input></label>
+            <label>Descripción: <input name="descripcion" type="text"></input></label>
+            <label>Precio: <input name="precio" type="number" min={0} step={.01} required></input></label>
+            <label>Stock: <input name="stock" type="number" min={0} required></input></label>
+            <button type="submit">Añadir</button>
         </form>
     )
 }
